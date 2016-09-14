9e3564a913caa726dd79b8fa954ab6cd85fe7a73
Rodrigo Kishi <rodrigokishi@usp.br>
2016-09-14 18:18:03 -0300

    Accepts larger files, but doesn't find keyframes anymore

--- src/main/java/intermidia/SimpleShotDetector.java
+++ src/main/java/intermidia/SimpleShotDetector.java
@@ -5,7 +5,6 @@ import java.io.FileWriter;
 
 import org.openimaj.image.MBFImage;
 import org.openimaj.video.Video;
-import org.openimaj.video.processing.shotdetector.VideoKeyframe;
 import org.openimaj.video.xuggle.XuggleVideo;
 
 import TVSSUnits.Shot;
@@ -14,39 +13,27 @@ import TVSSUtils.ShotSegmentator;
 
 public class SimpleShotDetector
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{	
 		Video<MBFImage> source = new XuggleVideo(new File(args[0]));
-		System.out.println("Determining shot boundaries.");
-		ShotList shotList = ShotSegmentator.segmentVideo(source);	
+		System.out.println("Determining shot boundaries.");		
+		ShotSegmentator shotSegmentator = new ShotSegmentator(source);
+		ShotList shotList = shotSegmentator.segmentVideo();	
 		source.close();
 		
 		System.out.println("Writing output.");
-		try
+
+		FileWriter shotWriter = new FileWriter(args[1]);
+		for(int i = 0; i < shotList.listSize(); i++)
 		{
-			FileWriter shotWriter = new FileWriter(args[1]);
-			FileWriter keyframeWriter = new FileWriter(args[2]);
-			for(int i = 0; i < shotList.listSize(); i++)
-			{
-				/*Write a shot on the shot segmentation file.*/
-				Shot shot = shotList.getShot(i);				
-				shotWriter.write(String.valueOf(shot.getStartBoundary().getTimecode().getFrameNumber()) + "\t" +
-								 String.valueOf(shot.getEndBoundary().getTimecode().getFrameNumber()) + "\n");
-								
-				for(int j = 0; j < shot.getKeyFrameList().size(); j++)
-				{
-					/*Write a keyframe on the keyframe list file*/
-					VideoKeyframe<MBFImage> keyframe = shot.getKeyFrameList().get(j);
-					keyframeWriter.write(i + "\t" + keyframe.getTimecode().getFrameNumber() + "\n");
-				}
-			}
-			shotWriter.close();
-			keyframeWriter.close();
-		}catch(Exception e)
-		{
-			System.out.println("Error: SimpleShotDetector/SimpleShotDetector/main()");
-			System.exit(1);  			
+			/*Write a shot on the shot segmentation file.*/
+			Shot shot = shotList.getShot(i);				
+			shotWriter.write(String.valueOf(shot.getStartBoundary().getTimecode().getFrameNumber()) + "\t" +
+							 String.valueOf(shot.getEndBoundary().getTimecode().getFrameNumber()) + "\n");
+							
 		}
+		shotWriter.close();
+
 		System.out.println("Successful execution.");
 	}
 
