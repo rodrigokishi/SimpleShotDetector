ef5b90cbe5ece4515cf3bf1449f4b7a52c0f7ef7
Rodrigo Kishi <rodrigokishi@usp.br>
2016-09-14 21:29:04 -0300

    Uncomment to produce keyframes. (not good)

--- src/main/java/intermidia/SimpleShotDetector.java
+++ src/main/java/intermidia/SimpleShotDetector.java
@@ -3,8 +3,6 @@ package intermidia;
 import java.io.File;
 import java.io.FileWriter;
 
-import org.openimaj.image.MBFImage;
-import org.openimaj.video.Video;
 import org.openimaj.video.xuggle.XuggleVideo;
 
 import TVSSUnits.Shot;
@@ -15,7 +13,7 @@ public class SimpleShotDetector
 {
 	public static void main(String[] args) throws Exception
 	{	
-		Video<MBFImage> source = new XuggleVideo(new File(args[0]));
+		XuggleVideo source = new XuggleVideo(new File(args[0]));
 		System.out.println("Determining shot boundaries.");		
 		ShotSegmentator shotSegmentator = new ShotSegmentator(source);
 		ShotList shotList = shotSegmentator.segmentVideo();	
@@ -24,15 +22,20 @@ public class SimpleShotDetector
 		System.out.println("Writing output.");
 
 		FileWriter shotWriter = new FileWriter(args[1]);
+		/*FileWriter keyframeWriter = new FileWriter(args[2]);*/
 		for(int i = 0; i < shotList.listSize(); i++)
 		{
 			/*Write a shot on the shot segmentation file.*/
 			Shot shot = shotList.getShot(i);				
 			shotWriter.write(String.valueOf(shot.getStartBoundary().getTimecode().getFrameNumber()) + "\t" +
 							 String.valueOf(shot.getEndBoundary().getTimecode().getFrameNumber()) + "\n");
-							
+			/*for(int j = 0; j < shot.getKeyFrameList().size(); j++)
+			{
+				keyframeWriter.write((i + 1) + "\t" + String.valueOf(shot.getKeyFrameList().get(j).getTimecode().getFrameNumber()) + "\n");
+			}*/
 		}
 		shotWriter.close();
+		//keyframeWriter.close();
 
 		System.out.println("Successful execution.");
 	}
